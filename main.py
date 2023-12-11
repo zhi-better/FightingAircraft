f117de92861d7f60e0bb93bc4dcc76a091b09574
最爱耀耀啦 <3167038449@qq.com>
2023-12-11 16:53:25 +0800

    解决地图随着战机位置更新的问题

--- main.py
+++ main.py
@@ -72,37 +72,69 @@ def run_game():
     # print(select_map_name)
     # =========================================================================
 
-    tile_ids, image_source, width, height, tile_width, tile_height = load_map(xml_file_path="map/map0.xml")
-    # 加载游戏图像资源
-    template_image = pygame.image.load(image_source)
-    start_point = 0 * width * tile_width, 0 * height * tile_height
-
     pygame.init()  # 初始化pygame
     game_window_size = 1080, 720  # 设置窗口大小
     screen = pygame.display.set_mode(game_window_size)  # 显示窗口
     pygame.display.set_caption("FightingAircraft")
+    clock = pygame.time.Clock()
     color = (0, 0, 0)  # 设置颜色
 
+    tile_ids, image_source, width, height, tile_width, tile_height = load_map(xml_file_path="map/map0.xml")
+    # 加载游戏图像资源
+    template_image = pygame.image.load(image_source)
+    start_point = np.array([0 * width * tile_width, 0 * height * tile_height])
+    x_load_block_count = int(np.ceil(game_window_size[0] * 0.5 / tile_width)) + 1
+    y_load_block_count = int(np.ceil(game_window_size[1] * 0.5 / tile_height)) + 1
+    step = 5
+
+    # 初始化按键状态字典
+    key_states = {pygame.K_UP: False, pygame.K_DOWN: False, pygame.K_LEFT: False, pygame.K_RIGHT: False}
 
     while True:
+        clock.tick(30)
         for event in pygame.event.get():  # 遍历所有事件
             if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
                 pygame.quit()  # 退出pygame
                 sys.exit(0)
 
+            # 处理键盘按下和释放事件
+            if event.type == pygame.KEYDOWN:
+                if event.key in key_states:
+                    key_states[event.key] = True
+            elif event.type == pygame.KEYUP:
+                if event.key in key_states:
+                    key_states[event.key] = False
+
+        # print(pygame.key.name(bools.index(1)))
+        if key_states[pygame.K_UP]:
+            start_point[1] -= step
+        elif key_states[pygame.K_DOWN]:
+            start_point[1] += step
+        elif key_states[pygame.K_LEFT]:
+            start_point[0] -= step
+        elif key_states[pygame.K_RIGHT]:
+            start_point[0] += step
+
+        # 清屏
+        screen.fill((255, 255, 255))
+        start_point[0] = start_point[0] % (width * tile_width)
+        start_point[1] = start_point[1] % (height * tile_height)
+        x_diff = start_point[0] % tile_width
+        y_diff = start_point[1] % tile_height
         # 开始加载图像并绘图
         x_tile_block = int(start_point[0] / tile_width)
         y_tile_block = int(start_point[1] / tile_height)
-        for i in range(x_tile_block-2, x_tile_block+2):
-            for j in range(y_tile_block-1, y_tile_block+1):
+        for i in range(x_tile_block-x_load_block_count, x_tile_block+x_load_block_count):
+            for j in range(y_tile_block-y_load_block_count, y_tile_block+y_load_block_count):
                 # 首先判断出该位置对应的 id 是多少
-                id = tile_ids[i*width+j]
+                id = tile_ids[(j % height) * width + (i % width)]
                 tile_rect = get_rect_from_tile_id(id, tile_width=tile_width, tile_height=tile_height)
                 screen.blit(template_image,
-                    (0.5*game_window_size[0]+(i-x_tile_block)*tile_width,
-                     0.5*game_window_size[1]+(j-y_tile_block)*tile_height), tile_rect)
+                    (0.5*game_window_size[0]+(i-x_tile_block)*tile_width - x_diff,
+                     0.5*game_window_size[1]+(j-y_tile_block)*tile_height - y_diff), tile_rect)
 
         pygame.display.flip()  # 更新全部显示
+        # print('refresh all')
 
 
 if __name__ == '__main__':
